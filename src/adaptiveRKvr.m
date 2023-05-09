b2d03722371a9d6d88d509c2b2a260ff609a77ce
yuki31okamoto <okamoto.yuki.75a@st.kyoto-u.ac.jp>
2023-05-09 19:44:20 +0900

    added while roop

--- src/adaptiveRKvr.m
+++ src/adaptiveRKvr.m
@@ -2,42 +2,43 @@ function [vr_err, vr_temp] = adaptiveRKvr(ddt, qr_ave_temp_idx, ParamRK, vr_idx,
     
     % %% Adaptive Runge-Kutta
     % % (1)
-    fr = funcr_vr4RK(vr_idx, qr_idx, hr_idx);   
+    [fr, vr_idx, qr_idx] = funcr_vr4RK(vr_idx, qr_idx, hr_idx);   
     vr_temp = vr_idx + ParamRK.b21 * ddt * fr;
     vr_temp(vr_temp<0) = 0;
     qr_ave_temp_idx = qr_ave_temp_idx + qr_idx * ddt;
-    
+    % disp(max(vr_temp))
+
     % % (2)
-    kr2 = funcr_vr4RK(vr_idx, qr_idx, hr_idx); 
+    [kr2, vr_idx, qr_idx] = funcr_vr4RK(vr_idx, qr_idx, hr_idx); 
     vr_temp = vr_idx + ddt * (ParamRK.b31 * fr + ParamRK.b32 * kr2);
     vr_temp(vr_temp<0) = 0;
     qr_ave_temp_idx = qr_ave_temp_idx + qr_idx * ddt;
     % 
     % % (3)
-    kr3 = funcr_vr4RK(vr_idx, qr_idx, hr_idx); 
+    [kr3, vr_idx, qr_idx] = funcr_vr4RK(vr_idx, qr_idx, hr_idx); 
     vr_temp = vr_idx + ddt * (ParamRK.b41 * fr + ParamRK.b42 * kr2 + ParamRK.b43 * kr3);
     vr_temp(vr_temp<0) = 0;
     qr_ave_temp_idx = qr_ave_temp_idx + qr_idx * ddt;
     % 
     % % (4)
-    kr4 = funcr_vr4RK(vr_idx, qr_idx, hr_idx); 
+    [kr4, vr_idx, qr_idx] = funcr_vr4RK(vr_idx, qr_idx, hr_idx); 
     vr_temp = vr_idx + ddt * (ParamRK.b51 * fr + ParamRK.b52 * kr2 + ParamRK.b53 * kr3 + ParamRK.b54 * kr4);
     vr_temp(vr_temp<0) = 0;
     qr_ave_temp_idx = qr_ave_temp_idx + qr_idx * ddt;
     % 
     % % (5)
-    kr5 = funcr_vr4RK(vr_idx, qr_idx, hr_idx); 
+    [kr5, vr_idx, qr_idx] = funcr_vr4RK(vr_idx, qr_idx, hr_idx); 
     vr_temp = vr_idx + ddt * (ParamRK.b61 * fr + ParamRK.b62 * kr2 + ParamRK.b63 * kr3 + ParamRK.b64 * kr4 + ParamRK.b65 * kr5);
     vr_temp(vr_temp<0) = 0;
     qr_ave_temp_idx = qr_ave_temp_idx + qr_idx * ddt;
     % 
     % % (6)
-    kr6 = funcr_vr4RK(vr_idx, qr_idx, hr_idx); 
+    [kr6, vr_idx, qr_idx] = funcr_vr4RK(vr_idx, qr_idx, hr_idx); 
     vr_temp = vr_idx + ddt * (ParamRK.c1 * fr + ParamRK.c3 * kr3 + ParamRK.c4 * kr4 + ParamRK.c6 * kr6);
     vr_temp(vr_temp<0) = 0;
     qr_ave_temp_idx = qr_ave_temp_idx + qr_idx * ddt;
     
     vr_err = ddt * (ParamRK.dc1 * fr + ParamRK.dc3 * kr3 + ParamRK.dc4 * kr4 + ParamRK.dc5 * kr5 + ParamRK.dc6 * kr6);
-
+   
 
 end
